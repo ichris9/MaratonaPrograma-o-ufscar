0eeed6e1cc09bd4bed5f97bf9e5034a57df38c9b
Christian Hugo Cavalcante dos Santos <859621@ufscar.br>
2026-05-25 13:22:12 -0300

    class 2: desafio do bondinho

---
+++ class 2/bondinho.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int A, M;
+    cin >> A >> M;
+
+    if((A+M)> 50){
+        cout<<"N";
+    }else{
+        cout<<"S";
+    }
+
+    return 0;
+}
