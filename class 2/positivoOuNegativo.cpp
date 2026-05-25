f1ca181654b788b1e525bdd8400e34e138fe1b32
Christian Hugo Cavalcante dos Santos <859621@ufscar.br>
2026-05-25 13:14:28 -0300

    class 2 - desafio positivo ou negativo

---
+++ class 2/positivoOuNegativo.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int x;
+    cin>>x;
+    
+    if(x<0){
+        cout<<"negativo";
+    }
+    else{
+        cout<<"positivo";
+    }
+    
+
+    return 0;
+}
